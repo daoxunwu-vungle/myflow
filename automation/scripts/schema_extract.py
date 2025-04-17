843447cfff88d898acba4b1f5d985649d7cebea2
Daoxun Wu <daoxun.wu@vungle.com>
2025-04-17 08:49:20 +0800

    add skeleton logic for schema extract

--- automation/scripts/schema_extract.py
+++ automation/scripts/schema_extract.py
@@ -1,4 +1,22 @@
 #! /usr/bin/env python
+'''
+This application will extract schema changes from 2 version of schema file
+
+Usage:
+    schema_extract.py (-h | --help)
+    schema_extract.py <new_version_file> <old_version_file> <output_file>
+
+Options:
+    -h, --help          Print this screen and exit.
+    <new_version_file>  New schema version file, contains new added fields.
+    <old_version_file>  Base schema file.
+    <output_file>       The file path which accept schema diff info.
+'''
+from docopt import docopt
+import sys
+
+def main(args):
+    print(f'new_version_file: {args["<new_version_file>"]} \t old_version_file: {args["<old_version_file>"]} \t output_file: {args["<output_file>"]}')
 
 if __name__ == "__main__":
-    print("runs schema_extract.py")
+    sys.exit(main(docopt(__doc__)))
